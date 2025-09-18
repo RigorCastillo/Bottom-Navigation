65c3c584874576a5242514a392162aae3bd1c788
Rigs <rigormiguelscastillo@gmail.com>
2025-09-18 17:54:48 +0800

    Flutter Bottom Navigation

---
+++ lib/pages/projects_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class  ProjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Projects page'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+    );
+  }
+}
